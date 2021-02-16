69d7fc42982e6e75505e87e5b34b2cb4575fc36e
Shubam Bhasin <33823641+shubambhasin@users.noreply.github.com>
2021-02-17 03:57:56 +0530

    feat: features added

--- src/Figma.js
+++ src/Figma.js
@@ -1,41 +1,158 @@
 import React, { useState } from "react";
-
+import "./styles.css";
 const Figma = () => {
-  const [heading, setHeading] = useState("1");
-  const [red, setRed] = useState("100");
-  const [green, setGreen] = useState("100");
-  const [blue, setBlue] = useState("100");
-  const [background, setBackground] = useState({
-    red: `red`,
-    green: `green`,
-    blue: `blue`,
-    yellow: `yellow`,
-    pink: `pink`,
-    white: `white`
-  });
+  const [heading, setHeading] = useState("2");
+  const [headingColor, setHeadingColor] = useState("");
+  // const [red, setRed] = useState("100");
+  // const [green, setGreen] = useState("100");
+  // const [blue, setBlue] = useState("100");
+  const [background, setBackground] = useState("");
+
+  const [borderThick, setBorderThick] = useState(1);
+
   const [borderColor, setBorderColor] = useState("#000");
-  const [borderRadius, setBorderRadius] = useState("0");
-  const [padding, setPadding] = useState("1");
+  const [borderRadius, setBorderRadius] = useState(1);
+  const [padding, setPadding] = useState(1);
+  const [paraColor, setParaColor] = useState("");
+  const [paraFont, setparaFont] = useState(10);
+
+  // ***************function*******************
+  const increaseBorder = () => {
+    setBorderThick((borderThick) => borderThick + 1);
+  };
+  const decreaseBorder = () => {
+    if (borderThick === 1) {
+      setBorderThick(1);
+    } else {
+      setBorderThick((borderThick) => borderThick - 1);
+    }
+  };
+
+  // #################radius*****************
+  const increaseBorderRadius = () => {
+    setBorderRadius((borderRadius) => borderRadius + 1);
+  };
+  const decreaseBorderRadius = () => {
+    if (borderRadius <= 1) {
+      setBorderRadius(0);
+    } else {
+      setBorderRadius((borderRadius) => borderRadius - 1);
+    }
+  };
+  const increasePadding = () => {
+    setPadding((padding) => padding + 1);
+  };
+  const decreasePadding = () => {
+    if (padding <= 1) setPadding(0);
+    else {
+      setPadding((padding) => padding - 1);
+    }
+  };
+  const setBackgroundFun = (e) => {
+    setBackground(e.target.value);
+  };
+  const headingColorChange = (e) => {
+    setHeadingColor(e.target.value);
+  };
+  const paraColorChange = (e) => {
+    setParaColor(e.target.value);
+  };
+  const paraFontIncrease = () => {
+    setparaFont(paraFont => paraFont +1)
+  };
+  const paraFontDecrease = () => {
+    if(paraFont ===1)
+    {
+      setparaFont(1)
+    }
+    else
+    {
+      
+    setparaFont(paraFont => paraFont -1)
+    }
+  };
+
+
   return (
-    <div class="figma-body">
+    <div className="figma-body">
+      <h2>Style as you want</h2>
+      <div className="figma-style">
+        <div className="border-style">
+          <span>
+            Border: <button onClick={increaseBorder}>Increase</button>
+            <button onClick={decreaseBorder}>Decrease</button>{" "}
+          </span>
+          <br />
+          <span>
+            Border-radius:<strong>{` ${borderRadius}px `}</strong>
+            <button onClick={increaseBorderRadius}>Increase</button>
+            <button onClick={decreaseBorderRadius}>Decrease</button>
+          </span>
+        </div>
+        <div className="padding-style">
+          Padding:<strong>{` ${padding}rem `}</strong>
+          <span>
+            <button onClick={increasePadding}>Increase</button>
+          </span>
+          <span>
+            <button onClick={decreasePadding}>Dncrease</button>
+          </span>
+        </div>
+        <div className="background-style">
+          Background Color-
+          <input type="color" onChange={setBackgroundFun} />
+          {/* <br />
+          <br />
+          {background.map((color) => {
+            return (
+              <span
+                style={{
+                  margin: "5px",
+                  backgroundColor: `${color}`,
+                  padding: "0.5rem",
+                  border:"1px solid black",
+                  borderRadius: "5px"
+                }}
+                onClick={setBackgroundFun}
+              >
+                {color}
+              </span>
+            );
+          })} */}
+        </div>
+        <div className="Heading-style">
+          Heading Color: <input type="color" onChange={headingColorChange} />
+        </div>
+        <div className="para-style">
+          ParagraphColor: <input type="color" onChange={paraColorChange} />
+          Paragraph text-size: {` ${paraFont}px `}{" "}
+          <button onClick={paraFontIncrease}>Increase</button>
+          {` `} <button onClick={paraFontDecrease}>Decrease</button>
+        </div>
+      </div>
       <div
         class="figma-change-box"
         style={{
-          backgroundColor: `${background.white}`,
-          border: `1px solid ${borderColor}`,
+          backgroundColor: `${background}`,
+          border: `${borderThick}px solid ${borderColor}`,
           borderRadius: `${borderRadius}px`,
-          padding: `${padding}rem`
+          padding: `${padding}rem`,
+          color: `${paraColor}`
         }}
       >
         <h1
           style={{
             fontSize: `${heading}rem`,
-            color: `rgb(${red}, ${green}, ${blue})`
+            color: `${headingColor}`
           }}
         >
           I am heading
         </h1>
-        <p>
+        <p
+          style={{
+            fontSize: `${paraFont}px`
+          }}
+        >
           I am paragraph, loremos benevolentiae pariter aequaliterque
           respondeat, tertiam, ut.
         </p>
